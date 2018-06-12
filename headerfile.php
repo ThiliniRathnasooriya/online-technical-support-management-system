86c54f44a3a33f2af7e56df96b18c013e042aa6e
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-06-12 18:33:30 +0530

    Include this file on your web pages
    First include header.php file
    then include headerfile.php file
    then write your body (don't type <body> tag again since it is included in header.php file
    after the content is written then include footer.php file
    then close body tag </body>
    then close html tag </html>

---
+++ headerfile.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="navbar">
+        <a href="./" class="logo">FIND LANKA</a>
+        <div class="rightnav">
+            <a class="active" href="./">Home</a>
+            <a href="#about">About Us</a>
+            <a href="#contact">Contact Us</a>
+            <a href="#services">Services</a>
+            <a href="#help">Help</a>
+            <a href="#login">Log in</a>
+        </div>
+    </div>
+</header>
