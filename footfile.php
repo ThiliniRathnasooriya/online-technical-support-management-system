3139e54ef23e9df0aca8c1833f254d44a80f8c3c
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-06-12 13:26:04 +0530

    First Commit
    Created Home page
    Include header.php and footer.php at your own webpages

---
+++ footfile.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer">
+      <p class="footerleft"> &copy; Find Lanka Incorporated. 2018 | All Rights Reserved.</p>
+      <div class="footerright">
+        <a href="#home">Link 1</a>
+        <a href="#about">Link 2</a>
+        <a href="#contact">Link 2</a>
+      </div>
+      <div style="clear: both;"></div>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    include 'header.php';
+?>
+
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+  <p> this is some sample text!</p>
+
+<?php
+    include 'footfile.php';
+?> 
+
+</body>
+</html>
