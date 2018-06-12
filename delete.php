294714984ec80007a2cd80f2d12f5d889ebc58ec
Nipuna Weerasekara <w.nipuna@gmail.com>
2018-06-12 18:56:09 +0530

    Minor edits
    Minor edits in the header file
    dont include userheader.php file in any web page unless it is a part of the admin page

--- delete.php
+++ delete.php
@@ -2,19 +2,11 @@
     include 'header.php';
 ?>
 
-		<header>
-			<div class="navbar">
-				<a href="./" class="logo">FIND LANKA</a>
-				<div class="rightnav">
-					<a href="./admin.php">Home</a>
-					<a class="active" href="./users.php">User Administration</a>
-					<a href="#contact">Customer Requests</a>
-					<a href="#services">Technicians</a>
-					<a href="#help">Reports</a>
-					<a href="#login">Log out</a>
-				</div>
-			</div>
-		</header>
+<?php
+    include 'userheader.php';
+?>
+
+		
 
 		<h1>Search & Delete</h1>
 		<form method="post" action="delete.php" class="center">
--- users.php
+++ users.php
@@ -2,19 +2,9 @@
     include 'header.php';
 ?>
 
-  <header>
-    <div class="navbar">
-      <a href="./" class="logo">FIND LANKA</a>
-      <div class="rightnav">
-        <a href="./admin.php">Home</a>
-        <a class="active" href="./users.php">User Administration</a>
-        <a href="#contact">Customer Requests</a>
-        <a href="#services">Technicians</a>
-        <a href="#help">Reports</a>
-        <a href="#login">Log out</a>
-      </div>
-    </div>
-  </header>
+<?php
+    include 'userheader.php';
+?>  
 
 	<h1>User authentication</h1>
 	<a href="create.php"><button type="button">Create profile</button></a><br><br>
